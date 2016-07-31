e0c5a7077ea366f78c4f2ca8bc104b9eccc9b705
Seth Banks <stb@subimage.com>
2016-07-30 22:24:43 -0700

    Create/kill droplets works reliably

--- cloud_proxy_generator.rb
+++ cloud_proxy_generator.rb
@@ -12,9 +12,126 @@ Dir.glob("lib/**/*.rb") {|f| require_relative f}
 class CloudProxyGenerator
   include Singleton
 
+  attr_accessor :config, :d_client, :db
+
+  STARTING_PROXY_PORT = 31337
+  CONFIG_FILE = 'config.yml'
+  DB_FILE = 'database.yml'
+
   def initialize
+    load_config
+    load_or_init_database
+    @d_client = DropletKit::Client.new(
+      access_token: @c_do['access_token']
+    )
+    @num_proxies = @config['num_proxies']
   end
 
-  def run
+  def create_proxies
+    create_droplets
+    create_ssh_tunnels
+    print_proxy_addresses
+    persist_database
   end
+
+  def kill_proxies
+    kill_ssh_tunnels
+    kill_droplets
+  end
+
+  # DROPLETS ------------------------------------------------------------------
+  def create_droplets
+    puts "CREATING DROPLETS"
+    (1..@num_proxies).each do |i|
+      d_name = "proxy-#{i}"
+      d = DropletKit::Droplet.new(
+        name:     d_name,
+        region:   @c_do['regions'].sample,
+        image:    @c_do['image_name'],
+        size:     @c_do['size'],
+        # Imperative that your local machine pub.key is already
+        # uploaded if you'd like this to work...
+        ssh_keys: @d_client.ssh_keys.all.collect { |k| k.fingerprint }
+      )
+      puts "  - #{d_name}"
+      created = @d_client.droplets.create(d)
+      add_droplet_to_db(created.id, d_name)
+    end
+    return droplets
+  end
+
+  def kill_droplets
+    puts "DELETING DROPLETS"
+    droplets.each do |id, attrs|
+      begin
+        puts "  - #{id} #{attrs['name']}"
+        @d_client.droplets.delete(id: id)
+      rescue DropletKit::Error => e
+        puts e.message
+        puts "...sleeping!"
+        sleep 5
+        retry
+      end
+    end
+  end
+
+  # SSH TUNNELS ---------------------------------------------------------------
+  def create_ssh_tunnels
+  end
+
+  def kill_ssh_tunnels
+  end
+
+  # Shortcut so we can query up to date distribution clients.
+  # without using CURL examples from the API reference.
+  def print_distros
+    d_client.images.all(type:'distribution').each do |i| 
+      # Not sure of the naming method they're using
+      # Looks to be this from the API docs...
+      possible_image_name = "#{namify(i.distribution)}-#{namify(i.name)}"
+
+      puts "= #{i.distribution} - #{i.name}"
+      puts "  #{possible_image_name}"
+    end
+    return nil
+  end
+
+  private
+
+  def namify(str)
+    str.strip.downcase.gsub(/[^a-z0-9]/i, "-")
+  end
+
+  def load_config
+    @config = YAML::load_file(CONFIG_FILE)
+    @c_do = @config['digital_ocean']
+  end
+
+  # Tries to load persisted objects from DB_FILE, 
+  # otherwise initialize objects we need.
+  def load_or_init_database
+    @db = YAML::load_file(DB_FILE) if File.exist?(DB_FILE)
+
+    @db ||= {}
+    @db['droplets'] ||= {}
+  end
+
+  def persist_database
+    File.open(DB_FILE, 'w') { |f| f.write(YAML.dump(@db)) }
+  end
+
+  def add_droplet_to_db(id, name)
+    droplets[id] = {
+      'name' => name,
+      'local_port' => STARTING_PROXY_PORT + (droplets.size+1)
+    }
+  end
+
+  def droplets
+    @db['droplets']
+  end
+
 end
+
+# For quick irb reference
+CPG = CloudProxyGenerator.instance
